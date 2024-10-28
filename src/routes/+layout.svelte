ca5dbeae584dfa535014d784f54896b0708f67cc
gralmeidan <gralmeidan@gmail.com>
2024-10-28 14:25:04 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '@fontsource-variable/sofia-sans';
+	import '@fontsource-variable/nunito';
+
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<header class="sticky w-full bg-primary p-3">
+	<span class="text-primary-content">InteraPC</span>
+</header>
+{@render children()}
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import aspectRatio from '@tailwindcss/aspect-ratio';
+import daisyui, { type Config as DaisyUIConfig } from 'daisyui';
+
+import type { Config } from 'tailwindcss';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			fontFamily: {
+				serif: 'Sofia Sans Variable',
+				nunito: 'Nunito Variable'
+			}
+		}
+	},
+
+	plugins: [aspectRatio, daisyui],
+	daisyui: {
+		themes: ['winter']
+	}
+} as Config & { daisyui: DaisyUIConfig };
