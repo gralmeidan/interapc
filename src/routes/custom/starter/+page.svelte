fc60cee52f3cf2249a3edb0944118a26b71dae59
gralmeidan <gralmeidan@gmail.com>
2024-11-01 14:53:44 -0400

    feat: add nav options to custom starter form

--- src/routes/custom/starter/+page.svelte
+++ src/routes/custom/starter/+page.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import { mQuestions } from '$lib/mocks';
 	import StarterQuestion from './components/StarterQuestion.svelte';
+	import MdiChevronLeftCircleOutline from '~icons/mdi/chevron-left-circle-outline';
+	import MdiArrowRight from '~icons/mdi/arrow-right';
 
 	let questions = $state(mQuestions);
 </script>
 
 <div class="flex h-lvh w-full flex-row justify-end">
-	<div class="_bg flex grow flex-col-reverse rounded-br-3xl rounded-tr-3xl bg-cover bg-right">
+	<div
+		class="_bg flex grow flex-col justify-between rounded-br-3xl rounded-tr-3xl bg-cover bg-right"
+	>
+		<a href="/" class="btn btn-ghost m-6 flex w-fit items-center text-xl font-semibold text-white">
+			<MdiChevronLeftCircleOutline class="h-10 w-10 text-white" />
+			Voltar para a home
+		</a>
 		<div aria-hidden="true" class="mx-auto w-fit py-2 text-[60px] font-semibold text-white">
 			InteraPC
 		</div>
@@ -21,6 +29,15 @@
 				}}
 			/>
 		{/each}
+		<div class="absolute bottom-0 right-0 p-6">
+			<button
+				class="btn btn-primary font-bold"
+				onclick={() => (window.location.href = '/custom/shop')}
+			>
+				Continuar
+				<MdiArrowRight class="h-4 w-4 text-white" />
+			</button>
+		</div>
 	</main>
 </div>
 
